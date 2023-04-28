0b80046eb1de04a3bbe21ffb49d3faeeee78ded5
gusttavocastilho <gustavocvalenciano@gmail.com>
2023-04-28 19:38:03 -0400

    refactor: move HttpClientSpy to its own file

--- tests/data/usecases/remote-get-by-puuid.spec.ts
+++ tests/data/usecases/remote-get-by-puuid.spec.ts
@@ -1,23 +1,8 @@
-import {
-  HttpClient,
-  HttpRequest,
-  HttpResponse,
-} from "../../../src/data/protocols/http";
 import { RemoteGetByPuuid } from "../../../src/data/usecases/get-by-puuid";
+import { HttpClientSpy } from "../mocks/mock-http";
 
 describe("RemoteGetByPuuid", () => {
   it("Should call HttpClient with correct URL", async () => {
-    class HttpClientSpy implements HttpClient {
-      url?: string;
-
-      async request(data: HttpRequest): Promise<HttpResponse<void>> {
-        this.url = data.url;
-        return {
-          statusCode: 200,
-        };
-      }
-    }
-
     const url = "any_url";
     const httpClient = new HttpClientSpy();
     const sut = new RemoteGetByPuuid(url, httpClient);
