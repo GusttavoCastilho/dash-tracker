fa674f098e3b5d40c478a1bd0f7e74b76c72e4c0
gusttavocastilho <gustavocvalenciano@gmail.com>
2023-04-28 19:50:22 -0400

    refactor: change RemoteGetByPuuid to use faker

--- tests/data/usecases/remote-get-by-puuid.spec.ts
+++ tests/data/usecases/remote-get-by-puuid.spec.ts
@@ -1,12 +1,13 @@
 import { RemoteGetByPuuid } from "../../../src/data/usecases/get-by-puuid";
 import { HttpClientSpy } from "../mocks/mock-http";
+import faker from 'faker';
 
 type SutTypes = {
   sut: RemoteGetByPuuid;
   httpClientSpy: HttpClientSpy;
 };
 
-const makeSut = (url = "any_url"): SutTypes => {
+const makeSut = (url = faker.internet.url()): SutTypes => {
   const httpClientSpy = new HttpClientSpy();
   const sut = new RemoteGetByPuuid(url, httpClientSpy);
 
@@ -18,7 +19,7 @@ const makeSut = (url = "any_url"): SutTypes => {
 
 describe("RemoteGetByPuuid", () => {
   it("Should call HttpClient with correct URL", async () => {
-    const url = "other_url";
+    const url = faker.internet.url();
     const { sut, httpClientSpy } = makeSut(url);
 
     await sut.getPuuid();
