aeaa2bd9b9d005a6d98029fb8b346b00c981e9b4
gusttavocastilho <gustavocvalenciano@gmail.com>
2023-04-28 19:48:00 -0400

    refactor: move SUT creation to a factory method

--- tests/data/usecases/remote-get-by-puuid.spec.ts
+++ tests/data/usecases/remote-get-by-puuid.spec.ts
@@ -1,12 +1,27 @@
 import { RemoteGetByPuuid } from "../../../src/data/usecases/get-by-puuid";
 import { HttpClientSpy } from "../mocks/mock-http";
 
+type SutTypes = {
+  sut: RemoteGetByPuuid;
+  httpClientSpy: HttpClientSpy;
+};
+
+const makeSut = (url = "any_url"): SutTypes => {
+  const httpClientSpy = new HttpClientSpy();
+  const sut = new RemoteGetByPuuid(url, httpClientSpy);
+
+  return {
+    sut,
+    httpClientSpy,
+  };
+};
+
 describe("RemoteGetByPuuid", () => {
   it("Should call HttpClient with correct URL", async () => {
-    const url = "any_url";
-    const httpClient = new HttpClientSpy();
-    const sut = new RemoteGetByPuuid(url, httpClient);
+    const url = "other_url";
+    const { sut, httpClientSpy } = makeSut(url);
+
     await sut.getPuuid();
-    expect(httpClient.url).toBe(url);
+    expect(httpClientSpy.url).toBe(url);
   });
 });
