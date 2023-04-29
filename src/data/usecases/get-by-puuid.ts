5491d472107e94d686a47c65a2b3ff50d6302033
gusttavocastilho <gustavocvalenciano@gmail.com>
2023-04-29 12:57:48 -0400

    feat: ensure RemoteGetByPuuid returns an GetByPuuid on success

--- src/data/usecases/get-by-puuid.ts
+++ src/data/usecases/get-by-puuid.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpStatusCode } from "@data/protocols/http";
 import { InvalidCredentialsError, UnexpectedError } from "@domain/errors";
 import { GetByPuuid } from "@domain/usecases";
 
-export class RemoteGetByPuuid {
+export class RemoteGetByPuuid implements GetByPuuid {
   constructor(
     private readonly url: string,
     private readonly httpClient: HttpClient<RemoteGetByPuuid.Model>
