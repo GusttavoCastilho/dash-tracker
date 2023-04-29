c5fa1495e68cfc56f480c23e05e5ee44b87c3533
gusttavocastilho <gustavocvalenciano@gmail.com>
2023-04-29 14:00:34 -0400

    test: ensure AxiosHttpClient returns correct response

--- tests/infra/http/axios-http-client.spec.ts
+++ tests/infra/http/axios-http-client.spec.ts
@@ -35,4 +35,16 @@ describe("AxiosHttpClient", () => {
       method: request.method,
     });
   });
+
+  it("Should return correct response", async () => {
+    const { sut, mockedAxios } = makeSut();
+
+    const httpResponse = await sut.request(mockHttpRequest());
+    const axiosResponse = await mockedAxios.request.mock.results[0].value;
+
+    expect(httpResponse).toEqual({
+      statusCode: axiosResponse.status,
+      data: axiosResponse.data,
+    });
+  });
 });
